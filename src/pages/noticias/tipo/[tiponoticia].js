f0c882f58a991fabfccd38a2ace4d76d069edaf1
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-07-07 18:00:53 +0000

    Alterado acesso a API

--- src/pages/noticias/tipo/[tiponoticia].js
+++ src/pages/noticias/tipo/[tiponoticia].js
@@ -1,7 +1,7 @@
 import { Container, Row } from "react-bootstrap";
 import Cards from "../../components/cards"
 export async function getServerSideProps(context) {
-    const res = await fetch(`http://localhost:3000/api/noticias/tipo/` + context.query.tiponoticia);
+    const res = await fetch(`https://portalnoticias-plum.vercel.app/api/noticias/tipo/` + context.query.tiponoticia);
     const repo = await res.json(res);
     return { props: { noticias: repo } }
 }
